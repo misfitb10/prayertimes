e1dc913dd6dfa4d99f89dee4e6d5d180142698e7
Bilal Hussain <bilal.hussain@incentro.com>
2018-05-25 18:00:00 +0200

    Removed 'build/generated' JS files, because we only want the 'core/src' TS files

--- util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    params: function (obj) {
-        return Object.keys(obj).map(function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]); }).join('&');
-    }
-};
